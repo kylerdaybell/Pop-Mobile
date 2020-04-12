1749619f9eed140c16fde114b6c0d9387bdea0f5
Kyler <kyler@pop-os.localdomain>
2020-04-12 14:39:13 -0600

    this is working we are talking to the api

--- popapp/Components/About.tsx
+++ popapp/Components/About.tsx
@@ -1,15 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState, FunctionComponent } from "react";
 import { TouchableOpacity, Text } from 'react-native'
-import { Actions } from 'react-native-router-flux'
+import { Actions } from 'react-native-router-flux';
+import APIUserService from '../Services/APIServices/APIUserService';
+import Token from '../Models/TokenModel';
+import User from '../Models/UserModel';
 
 const About = () => {
+
+
+    const [ThisToken, setThisToken] = useState(new Token(""));
+    const [Registered, setRegistered] = useState("");
+    const [Deleted, setDeleted] = useState("");
+
+    const HomePage = async() =>{
+        await UserRegister();
+        let token = await UserLogin();
+        await CreatePost(token);
+        //await UserDelete(token);
+    }
+
+    const CreatePost = async(token: Token) =>{
+        let post = new Post(0,0,"Kylers post","this is the content of kylers post")
+        let apipostservice = new APIPostService()
+        if(await apipostservice.Create(post,token)){
+            setDeleted("User Account Deleted") 
+        } else 
+        {
+            setDeleted("User Account Deleted Not Deleted")
+        }
+    }
+
+    const UserLogin = async() =>{
+        let apiservice = new APIUserService()
+        let user = new User(0,"kyler.daybell@gmail.com","kyler","admin")
+        let token = await apiservice.Login(user);
+        if(token){
+            setThisToken(token);
+            return token;
+        }else{
+            setThisToken(new Token(""));
+            return new Token("");
+        }
+    }
+
+
+    const UserDelete = async(token:Token) =>{
+        let apiservice = new APIUserService()
+        let user = new User(0,"kyler.daybell@gmail.com","kyler","user")
+        console.warn(token)
+        if(await apiservice.Delete(user,token)){
+            setDeleted("User Account Deleted") 
+        } else 
+        {
+            setDeleted("User Account Deleted Not Deleted")
+        }
+    }
+
+    const UserRegister = async() =>{
+        let apiservice = new APIUserService()
+        let user = new User(0,"kyler.daybell@gmail.com","kyler","admin")
+        if(await apiservice.Register(user)){
+            setRegistered("you are registered") 
+        } else 
+        {
+            setRegistered("you are not registered")
+        }
+    }
+
+    useEffect(() => {
+        HomePage();
+      }, []);
+
+
    const goToHome = () => {
       Actions.home()
    }
    return (
-      <TouchableOpacity style = {{ margin: 128 }} onPress = {goToHome}>
-         <Text>This is ABOUT</Text>
-      </TouchableOpacity>
+       <>
+        <TouchableOpacity style = {{margin: 128,backgroundColor:'#aaa' }} onPress = {goToHome}>
+        <text>Go ome</text>
+        </TouchableOpacity>
+        <Text>{ThisToken.key}</Text>
+       </>
    )
 }
 export default About
